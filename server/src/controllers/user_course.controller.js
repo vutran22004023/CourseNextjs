7eb39fb7cd19c5e06a706d30a61b522f935c6258
Nguyen Lam <nlsc1999@gmail.com>
2024-08-11 13:17:23 +0700

    update user course feature

--- server/src/controllers/user_course.controller.js
+++ server/src/controllers/user_course.controller.js
@@ -2,14 +2,14 @@ import { UserCourseService } from '../services/index.js';
 
 class UserCourseController {
   // Get user-course
-  async getUserCourse(req, res) {
-    try {
-      const result = await UserCourseService.getUserCourse(req.params);
-      res.status(200).json(result);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  }
+  // async getUserCourse(req, res) {
+  //   try {
+  //     const result = await UserCourseService.getUserCourse(req.params);
+  //     res.status(200).json(result);
+  //   } catch (error) {
+  //     res.status(500).json({ message: error.message });
+  //   }
+  // }
 
   // Start user-course
   async startUserCourse(req, res) {
@@ -25,6 +25,16 @@ class UserCourseController {
   async updateProgress(req, res) {
     try {
       const result = await UserCourseService.updateProgress(req.body);
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+  
+  // Get course progress
+  async getCourseProgress(req, res) {
+    try {
+      const result = await UserCourseService.getCourseProgress(req.user);
       res.status(200).json(result);
     } catch (error) {
       res.status(500).json({ message: error.message });
--- server/src/routes/user_course.route.js
+++ server/src/routes/user_course.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import UserCourseController from '../controllers/user_course.controller.js';
+import { passportMiddleware } from '../middlewares/index.js';
 
 const router = express.Router();
 
-router.post('/start-course', UserCourseController.startUserCourse);
-router.post('/update-progress', UserCourseController.updateProgress);
+router.post('/start-course', passportMiddleware, UserCourseController.startUserCourse);
+router.post('/update-progress', passportMiddleware, UserCourseController.updateProgress);
+router.get('/course-progress', passportMiddleware, UserCourseController.getCourseProgress);
 
 export default router;
