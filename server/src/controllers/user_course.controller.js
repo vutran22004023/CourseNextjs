d3d684ac1d009c3876983c3887f911a08a3c3c9a
nlsc1999 <nlsc1999@gmail.com>
2024-09-08 05:30:23 +0000

    - clean code

--- server/src/controllers/user_course.controller.js
+++ server/src/controllers/user_course.controller.js
@@ -1,16 +1,6 @@
 import { UserCourseService } from '../services/index.js';
 
 class UserCourseController {
-  // Get user-course
-  async getUserCourse(req, res) {
-    try {
-      const result = await UserCourseService.getUserCourse(req.params);
-      res.status(200).json(result);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  }
-
   // Start user-course
   async startUserCourse(req, res) {
     try {
