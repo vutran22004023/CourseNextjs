67fb063c6707a85649610714828a46f9307554b1
VuTran <levu1962004@gmail.com>
2024-10-19 01:04:40 +0700

    Ui and Api show room

--- src/app/online-learning/layout.tsx
+++ src/app/online-learning/layout.tsx
@@ -7,11 +7,11 @@ export default function RootLayout({
 }) {
   return (
     <div className="block md:flex w-full">
-    <div className="bg-background font-sans antialiased w-full flex gap-4 mt-5">
-      <div className="w-[8%] h-[calc(100v-200px)]">
+    <div className="bg-background font-sans antialiased w-full flex gap-4 mt-5 h-full">
+      <div className="w-[8%] h-[calc(90.5vh)]">
         <SidebarOnline/>
       </div>
-      <div className='w-[90%] mt-5 overflow-y-auto'>{children}</div>
+      <div className='w-[90%]'>{children}</div>
     </div>
     </div>
   );
--- src/app/online-learning/page.tsx
+++ src/app/online-learning/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import Button from "@/components/Button/Button";
+import CardRoom from "./cardRoom";
 export default function page() {
   return (
-    <div>
-      <div>dasdasdsa</div>
+    <div className="mt h-full overflow-y-auto">
+      <div className="flex gap-4 mt-10">
+        <div className="w-[80%] shadow-xl p-5 rounded-xl">
+          <CardRoom title="Đang diễn ra" />
+        </div>
+        <div className="flex-auto">
+          <Button
+            type="courseHeader"
+            className="p-2 flex flex-row justify-center"
+          >
+            Tạo lớp học
+          </Button>
+          <Button
+            type="courseHeader"
+            className="p-2 flex flex-row justify-center mt-2"
+          >
+            Tham gia lớp học ngày
+          </Button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
