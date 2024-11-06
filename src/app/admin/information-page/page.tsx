4792e4ecf0474cab8538f9897f25ad1b4baf541f
VuTran <levu1962004@gmail.com>
2024-11-06 14:30:49 +0700

    add form update footer

--- src/app/admin/information-page/page.tsx
+++ src/app/admin/information-page/page.tsx
@@ -4,11 +4,8 @@ import Text from "@/components/Text/text";
 export default function Informationpage() {
   return (
     <div className="container w-full">
-      <div className="mb-3">
-        <Text type="header">Thông tin Web</Text>
-      </div>
       <div className="w-full flex items-center justify-center">
-        <div className="flex items-center w-full md:w-[80%] justify-center p-5  rounded-2xl border-2 border-black">
+        <div className="flex items-center w-full md:w-[80%] justify-center">
           <InformationPageForm />
         </div>
       </div>
